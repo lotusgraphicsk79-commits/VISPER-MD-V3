14e8834fa9b6400f72a889e06142a821923e755d
lotusgraphicsk79-commits <lotusgraphicsk79@gmail.com>
2025-08-09 16:31:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&yoJQHSqJ#21iGW3lLe5rhF7tUcgdzKGiv8zbOYwJUtIvliv9OBXM' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&LgRimYCL#MEYH-t6OjURmcgMobizB4fLOEUabb8Hny1egs-bWr4M' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
